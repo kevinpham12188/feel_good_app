7d2b5cde3601346d2ebca16016dd0ceb722c69e6
Kevin Pham <97306391+kevinpham12188@users.noreply.github.com>
2022-02-17 02:23:14 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,94 @@
+import json
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.animation import Animation
+from hoverable import HoverBehavior
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
+from datetime import datetime
+from pathlib import Path
+import glob
+import random
+
+Builder.load_file('design.kv')
+
+
+class LoginScreen(Screen):
+    def sign_up(self):
+        self.manager.current = "sign_up_screen"
+
+    def go_to_pword(self):
+        self.manager.current = "forgot_password_screen"
+
+    def login(self, username, password):
+        with open("users.json") as file:
+            users = json.load(file)
+            if username in users and users[username]["password"] == password:
+                self.manager.current = "login_screen_success"
+            else:
+                self.ids.login_wrong.text = "Wrong username or password!"
+
+
+class RootWidget(ScreenManager):
+    pass
+
+
+class SignUpScreen(Screen):
+    def add_user(self, username, password):
+        with open("users.json") as file:
+            users = json.load(file)
+
+        users[username] = {"username": username,
+                           "password": password,
+                           "created": datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
+        with open("users.json", "w") as file:
+            json.dump(users, file)
+        self.manager.current = "sign_up_screen_success"
+
+
+class ForgotPasswordScreen(Screen):
+    def get_password(self, username):
+        with open("users.json") as file:
+            users = json.load(file)
+        if username in users:
+            x = users[username]
+            self.ids.your_password.text = x['password']
+            print(x["password"])
+
+
+class SignUpScreenSuccess(Screen):
+    def go_to_login(self):
+        self.manager.transition.direction = "right"
+        self.manager.current = "login_screen"
+
+
+class LoginScreenSuccess(Screen):
+    def log_out(self):
+        self.manager.transition.direction = "right"
+        self.manager.current = "login_screen"
+
+    def get_quote(self, feeling):
+        feeling = feeling.lower()
+        availabel_feelings = glob.glob("quotes/*txt")
+
+        availabel_feelings = [Path(filename).stem for filename in availabel_feelings]
+        if feeling in availabel_feelings:
+            with open(f"quotes/{feeling}.txt") as file:
+                quotes = file.readlines()
+            self.ids.quote.text = random.choice(quotes)
+        else:
+            self.ids.quote.text = "Try another feeling"
+
+
+class ImageButton(ButtonBehavior, HoverBehavior, Image):
+    pass
+
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+
+
+if __name__ == "__main__":
+    MainApp().run()
